bb7697b9856c38b0d7f0e6260bdf23750831ede5
pro028 <pro028@uit.no>
2025-02-24 15:01:10 +0100

    Small changes and some additional requirements, test.py will now download the speech_commands dataset and print the overall structure on completion

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 import numpy as np
 import tensorflow as tf
+import keras
+from datasets import Dataset, load_dataset
 
 print("Tensorflow version:", tf.__version__)
+print("Keras version:", keras.__version__)
 print("Numpy version:", np.__version__)
+
+dataset = load_dataset("google/speech_commands", "v0.01")
+
+# Overview of the dataset
+print(dataset)
