d10c546da8d663e39d2121ea94afb459da45c264
Kaja Skraamo <rub017@uib.no>
2025-04-23 16:17:17 +0200

    somthing new

--- model/models/baseline_model.py
+++ model/models/baseline_model.py
@@ -2,10 +2,10 @@ from tensorflow.keras import layers, models
 
 def build_baseline_model(input_shape, num_labels):
     model = models.Sequential([
-    layers.Input(shape=input_shape),
-    layers.Rescaling(1./255),
-    layers.Flatten(),
-    layers.Dense(32, activation='relu'),
-    layers.Dense(num_labels, activation='softmax')
-])
+        layers.Input(shape=input_shape),
+        layers.Rescaling(1./255),
+        layers.Flatten(),
+        layers.Dense(32, activation='relu'),
+        layers.Dense(num_labels, activation='softmax')
+    ])
     return model
